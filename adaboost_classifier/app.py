fbe2b878a4ee611ed85716e4b5b86d82cf89024b
Yanala Indhu <yanalaindhu11@gmail.com>
2026-05-31 13:02:56 +0530

    Update model path and add prediction error message

--- adaboost_classifier/app.py
+++ adaboost_classifier/app.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(
     BASE_DIR,
     "models",
-    "adaboost_classifier.pkl"
+    "adaboost_model.pkl"
 )
 
 # Load Model
